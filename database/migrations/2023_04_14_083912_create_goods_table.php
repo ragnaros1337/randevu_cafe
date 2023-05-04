5c0ac9baebe5b4302788a52de7132c2faa535dae
ragnaros1337 <torus.gothic@gmail.com>
2023-05-04 14:02:18 +0300

    1

---
+++ database/migrations/2023_04_14_083912_create_goods_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goods', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('article');
+            $table->text('description')->nullable();
+            $table->string('preview_image');
+            $table->decimal('price');
+            $table->integer('count');
+            $table->boolean('is_published')->default(true);
+            $table->foreignId('category_id')->nullable()->index()->constrained('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goods');
+    }
+};
